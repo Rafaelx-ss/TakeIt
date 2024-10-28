58c02e39d9bdafeba78ca6f495e776bff84a2e5b
Ariel <108147166+Arielo16@users.noreply.github.com>
2024-10-27 19:07:47 -0600

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import TakeItLogo from "./TakeItLogo";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-background dark:bg-gray-100">
+        <footer className="bg-surface">
             <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
                 <div>
