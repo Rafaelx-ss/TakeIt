a9de053a0e145dd1f1ab292404bc7baa21df96eb
Rafaelx-ss <solisrafael895@gmail.com>
2024-12-13 14:07:26 -0600

    Refactor EventosPage to load events from API and implement loading state. Removed mock data and added error handling for event fetching.

---
+++ types/eventos.ts
@@ -0,0 +1,11 @@
+export interface Evento {
+    eventosID: number
+    nombreEvento: string
+    fechaEvento: string
+    horaEvento: string
+    lugarEvento: string
+    direccionEvento: string
+    costoEvento: number
+    maximoParticipantesEvento: string
+    activoEvento: boolean
+} 
